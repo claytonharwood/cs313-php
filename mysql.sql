b6b82427353757fc5eee64ef4f1797d012013a01
Clayton Harwood <bigfatweb@gmail.com>
2018-10-19 21:46:11 -0600

    project 1

--- mysql.sql
+++ mysql.sql
@@ -5,8 +5,14 @@ CREATE TABLE player (
 );
 
 CREATE TABLE cardInfo (
-	playerID SERIAL primary key,
+	playerID primary key,
 	year VARCHAR(4),
 	cardType VARCHAR(50),
 	cardNumber VARCHAR(10)	
 );
+
+INSERT INTO player (playerId, FirstName, LastName) VALUES ('1', 'Freddie', 'Freeman');
+
+INSERT INTO cardInfo (playerId, year, cardType, cardNumber) VALUES ('1', '2011', 'Topps', '184');
+INSERT INTO cardInfo (playerId, year, cardType, cardNumber) VALUES ('1', '2013', 'Bowman', '68');
+INSERT INTO cardInfo (playerId, year, cardType, cardNumber) VALUES ('1', '2018', 'Gypsy Queen', '05');
---
+++ web/project1/index.php
@@ -0,0 +1,69 @@
+<?php
+    // Connect to database
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+?>
+
+<!DOCTYPE html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Sports Card Tracker</title>
+		<meta name="description" content="Track your sports cards!" />
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<script src="js/analytics.js" type="text/javascript"></script>
+				
+	</head>
+	<body>
+		<div class="top">
+			<div class="logo"></div>
+			<div id="phone-num"></div>
+			<nav>
+				<ul>
+					<li><a href=""> Home </a></li>
+					<li><a href="#products">Add Cards</a></li>
+				</ul>
+			</nav>
+		</div>	
+		<div class="bg"></div>
+		<div id="page">	
+		<?php  	
+    	$statement = $db->query('SELECT year, cardType, cardNumber FROM cardInfo');
+		while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+		{
+		  echo $row['year'] . ' ' . $row['cardType'] . ' ' . $row['cardNumber'] . '<br/>';
+		}
+		?>
+
+		</div>	
+
+		<footer>
+			<div id="bottom-nav">
+				
+				<div>&copy 2018 www.sportscardtracker.com, all rights reserved</div>
+				
+			</div>
+				
+		</footer>
+		
+	</body>
+</html>
