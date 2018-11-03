bc9ed90ee3e366c5c0460d59fbc0478155c84349
Clayton Harwood <bigfatweb@gmail.com>
2018-11-03 15:02:49 -0600

    db update

--- web/project1/addcards-index.php
+++ web/project1/addcards-index.php
@@ -1,8 +1,27 @@
 <?php
 
-// Create or access a Session
-session_start();
+ // Connect to database
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
 
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
 
 
 $action = filter_input(INPUT_POST, 'action');
