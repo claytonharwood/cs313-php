8e4c0aff723f7310e709b73627f189bc89443be0
Clayton Harwood <bigfatweb@gmail.com>
2018-11-03 19:30:54 -0600

    updated2

--- web/project1/index.php
+++ web/project1/index.php
@@ -43,22 +43,6 @@
 	       }
 	       ?>
 
-		<?php
-		$statement = $db->prepare("SELECT playerid, year, cardtype, cardnumber FROM cardInfo");
-		$statement->execute();
-		// Go through each result
-		while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-		{
-			// The variable "row" now holds the complete record for that
-			// row, and we can access the different values based on their
-			// name
-			echo '<p>';
-			echo '<strong>' . $row['playerid'] . ' ' . $row['year'] . ':';
-			echo $row['cardtype'] . '</strong>' . ' - ' . $row['cardnumber'];
-			echo '</p>';
-		}
-		?>
-
 		</div>	
 
 		<footer>
