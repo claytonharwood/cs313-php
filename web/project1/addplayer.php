c1af50db040618c0f782faf3413cd889543dbce7
Clayton Harwood <bigfatweb@gmail.com>
2018-11-05 20:26:43 -0700

    add player update

--- web/project1/addplayer.php
+++ web/project1/addplayer.php
@@ -3,18 +3,16 @@
 include './db.php';
 
 // get the data from the POST
-$playerid = $_POST['playerid'];
 $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
 
 try
 {
 	
-	$query = 'INSERT INTO player(playerid, firstname, lastname) VALUES(:playerid, :firstname, :lastname)';
+	$query = 'INSERT INTO player(firstname, lastname) VALUES(:firstname, :lastname)';
 	$statement = $db->prepare($query);
 	// Now we bind the values to the placeholders. This does some nice things
 	// including sanitizing the input with regard to sql commands.
-	$statement->bindValue(':playerid', $playerid);
 	$statement->bindValue(':firstname', $firstname);
 	$statement->bindValue(':lastname', $lastname);
 	$statement->execute();
