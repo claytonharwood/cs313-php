84cb75ce0e39fe52c2460327922890fb84022d6a
Clayton Harwood <bigfatweb@gmail.com>
2018-12-08 20:53:21 -0700

    updated js

--- web/project2/server.js
+++ web/project2/server.js
@@ -1,11 +1,16 @@
+var path = require('path');
 var express = require('express');
-
 var app = express();
 
 app.use(express.static("public"));
 
 app.set("views", "views");
 app.set("view engine", "ejs");
+
+app.use(express.json() );      
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 // index page 
 app.get('/', function(req, res) {
